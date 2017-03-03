7f9bbf66e123d7aa26921de47c97a0ae31bd2e96
Stephen Hussey <shussey@yahoo.com>
2017-03-03 15:24:28 -0800

    test

--- routes/segment.js
+++ routes/segment.js
@@ -6,39 +6,54 @@ var email = "bs@initech.com";
 var greeting = "Hola";
 
 exports.identify = function() {
-  
-  console.log('Seg: Indentify');
-  analytics.identify({
-    userId: userId,
-    traits: {
-      name: userName,
-      email: email,
-      createdAt: new Date('2016-06-14T02:00:19.467Z')
-    }
-  });
+
+    console.log('Seg: Indentify');
+    analytics.identify({
+        userId: userId,
+        traits: {
+            name: userName,
+            email: email,
+            createdAt: new Date('2016-06-14T02:00:19.467Z')
+        }
+    });
 }
 
 exports.track = function(eventName) {
 
-  console.log('Segment: track - ' + eventName);
-  analytics.track({
-    userId: userId,
-    event: eventName,
-    properties: {
-      greeting: greeting
-    }
-  });
+    console.log('Segment: track - ' + eventName);
+    analytics.track({
+        userId: userId,
+        event: eventName,
+        properties: {
+            greeting: greeting,
+            sourceDatasets: {
+                bytesRead: 2000,
+                bytesReadIncremental: 0,
+                columnsRead: 10,
+                name: "/user/icsuser/testDirectory/accounts.csv",
+                rowsRead: 8,
+                secureGateway: false,
+                type: "db2"
+            }
+        }
+    });
 }
 
-exports.page = function(eventName,url) {
 
-  console.log('Segment: track - ' + eventName);
-  analytics.page({
-    userId: userId,
-    type: "page",
-    name: eventName,
-    properties: {
-      url: url
-    }
-  });
+
+
+
+
+
+exports.page = function(eventName, url) {
+
+    console.log('Segment: track - ' + eventName);
+    analytics.page({
+        userId: userId,
+        type: "page",
+        name: eventName,
+        properties: {
+            url: url
+        }
+    });
 }
