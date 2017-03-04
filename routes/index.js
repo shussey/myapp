3eca19812ce051d22d0e7c345d2aba49ebefdb65
Stephen Hussey <shussey@yahoo.com>
2017-03-03 16:07:42 -0800

    1

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,26 @@ var router = express.Router();
 /* GET home page. */
 
 
-exports.index = router.get('/', function(req, res){
-  var pageName = "Main Page"
-  //segment.page(pageName,'/');
-  res.render('index', { title: pageName });
-  segment.track(pageName);
+exports.index = router.get('/', function(req, res) {
+    var pageName = "A Main Page"
+        //segment.page(pageName,'/');
+    res.render('index', { title: pageName });
+    segment.track(pageName);
 
 });
 
-exports.users = router.get('/users', function(req, res){
-  var pageName = "User Page"
-  //segment.page(pageName,'/users');
-  res.render('users', { title: pageName });
-  segment.track(pageName);
+exports.users = router.get('/users', function(req, res) {
+    var pageName = "A User Page"
+        //segment.page(pageName,'/users');
+    res.render('users', { title: pageName });
+    segment.track(pageName);
 
 });
 
-exports.logon = router.get('/logon', function(req, res){
-  var pageName = "Logon Page"
-  //segment.page(pageName,'/logon');
-  res.render('logon', { title: pageName });
-  segment.track(pageName);
+exports.logon = router.get('/logon', function(req, res) {
+    var pageName = "A Logon Page"
+        //segment.page(pageName,'/logon');
+    res.render('logon', { title: pageName });
+    segment.track(pageName);
 
 });
-
